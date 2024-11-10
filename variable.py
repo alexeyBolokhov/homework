136f625c78e683a7847c03f3ceba655b67410880
alexeyBolokhov <bolokhov.ae@gmail.com>
2024-11-10 18:44:44 +0500

    10/11

---
+++ variable.py
@@ -0,0 +1,6 @@
+count_lesson = 12 # Количество выполненных ДЗ
+count_time = 1.5 # Количество затраченных часов
+lesson = 'Python'
+one_lesson = 1.5 / 12  # время на 1 задание (часы делим на количество)
+print(one_lesson)
+print(lesson, ' всего задач: ',str(count_lesson) + ';', 'затрачено часов: ',str(count_time) + ';', 'среднее время выполнения: ' ,one_lesson,  ' часа!')
