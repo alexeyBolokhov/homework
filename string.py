cd7940e012041dbf1bb2bd6158b0a153dd5d82d7
alexeyBolokhov <bolokhov.ae@gmail.com>
2024-11-11 22:14:12 +0500

    11/11 string2

--- string.py
+++ string.py
@@ -1,13 +1,16 @@
-example = "Топинамбур"
+example = "Топинамбуры" #
 print(example[0]) # 1й символ
 print(example[-1]) # последний символ
 #####
 print(len(example)) # длина строки
-dlinna_str = len(example)//2 # половина строки чтобы без остатка
+dlinna_str = len(example) //2 # половина строки чтобы без остатка
 print(dlinna_str)
-#####
-print(example[dlinna_str:]) # вторая половина
 print(example[0:dlinna_str]) # первая половина
+print(example[dlinna_str:]) # вторая половина
+#####
+print(example[len(example)//2:]) # вторая половина
+#####
+print(example[5:]) # вторая половина
 print(example[::-1]) # слово наоборот
  # если с индекса[0] то всегда с 1го символа будет начитать Т
 ## Пример: 'Топинамбур'->'оиабр' здесь не берется 1й символ
