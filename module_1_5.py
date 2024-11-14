ee88fb60ba0f6a8ba0e8ba0e59eab29d36228731
alexeyBolokhov <bolokhov.ae@gmail.com>
2024-11-14 20:53:35 +0500

    14/11 list

---
+++ module_1_5.py
@@ -0,0 +1,22 @@
+immutable_var = (24, 11, 'год', 'месяц')
+print(immutable_var)
+print(type(immutable_var))
+#####
+#immutable_var[1] = 10 # хотим поменять (11 на 10) в кортеже
+#print(immutable_var)  # будет ошибка
+# кортеж - неизменяемый
+mutable_list = [24, 11, 'год', 'месяц']
+print(mutable_list)
+print(type(mutable_list))
+mutable_list[3] = 'зима' # хотим поменять (месяц на зима) в списке
+print(mutable_list)
+#список - можно менять
+#########
+mutable_list.append(2024) # обавили в конец
+print(mutable_list)
+mutable_list.extend(['река',88,'озеро']) # добавили другим методом (элементы  в [ ] )
+print(mutable_list)
+mutable_list.extend('озеро') # в отдельные символя разобрал и добавил в конец
+print(mutable_list)
+print('лужа' in mutable_list) # является ли Лужа элементом списка?
+print('месяц' in immutable_var) # является ли месяц элементом кортежа?
