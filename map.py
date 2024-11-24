ee14213621d513adfd7b77aeb3538fae93761736
alexeyBolokhov <bolokhov.ae@gmail.com>
2024-11-24 19:06:19 +0500

    24/11 start

---
+++ map.py
@@ -0,0 +1,318 @@
+import json
+
+Persei_TL = {
+    "TL": "Auriga_BR",
+    "TR": "Sadatoni_BR",
+    "BR": "Sirius_BR",
+    "BL": "Helvetios_TR",
+}
+Persei_TR = {
+    "TL": "Sadatoni_TL",
+    "TR": "Sadatoni_BR",
+    "BR": "Helvetios_BL",
+    "BL": "Volantis_TR",
+}
+Persei_BR = {
+    "TL": "Cygni_TL",
+    "TR": "Eridani_BR",
+    "BR": "Auriga_TL",
+    "BL": "ATLAS_C_BR",
+}
+Persei_BL = {
+    "TL": "ATLAS_A_TR",
+    "TR": "Cygni_BR",
+    "BR": "Auriga_TL",
+    "BL": "ATLAS_A_TL",
+}
+Sadatoni_TL = {
+    "TL": "Volantis_TL",
+    "TR": "Aquila_TR",
+    "BR": "Maia_TL",
+    "BL": "Persei_TR",
+}
+Sadatoni_TR = {
+    "TL": "Volantis_TR",
+    "TR": "Maia_BR",
+    "BR": "Cygni_BR",
+    "BL": "Sirius_TR",
+}
+Sadatoni_BR = {
+    "TL": "Persei_TR",
+    "TR": "Aquila_TR",
+    "BR": "Eridani_BR",
+    "BL": "Persei_TL",
+}
+Sadatoni_BL = {
+    "TL": "ATLAS_A_BL",
+    "TR": "Bootes_BL",
+    "BR": "Orion_BL",
+    "BL": "Alcyone_BL",
+}
+Sirius_TL = {
+    "TL": "Aquila_BR",
+    "TR": "ATLAS_B_BR",
+    "BR": "Eridani_TL",
+    "BL": "Eridani_TR",
+}
+Sirius_TR = {
+    "TL": "Sadatoni_TR",
+    "TR": "Eridani_BL",
+    "BR": "Auriga_TL",
+    "BL": "ATLAS_A_BR",
+}
+Sirius_BR = {
+    "TL": "Persei_TL",
+    "TR": "Eridani_TR",
+    "BR": "Volantis_BL",
+    "BL": "ATLAS_B_TR",
+}
+Sirius_BL = {
+    "TL": "Maia_BL",
+    "TR": "Alcyone_BL",
+    "BR": "Helvetios_BL",
+    "BL": "Cygni_BR",
+}
+Volantis_TL = {
+    "TL": "Volantis_BL",
+    "TR": "ATLAS_C_BL",
+    "BR": "Helvetios_BR",
+    "BL": "Sadatoni_TL",
+}
+Volantis_TR = {
+    "TL": "Sadatoni_TR",
+    "TR": "Auriga_BL",
+    "BR": "Bootes_BR",
+    "BL": "Persei_TR",
+}
+Volantis_BR = {
+    "TL": "Eridani_BL",
+    "TR": "Orion_TR",
+    "BR": "ATLAS_A_TL",
+    "BL": "Auriga_BL",
+}
+Volantis_BL = {
+    "TL": "Helvetios_TR",
+    "TR": "Sirius_BR",
+    "BR": "Volantis_TL",
+    "BL": "Eridani_TL",
+}
+ATLAS_A_TL = {
+    "TL": "Volantis_BR",
+    "TR": "Bootes_BL",
+    "BR": "Helvetios_TR",
+    "BL": "Persei_BL",
+}
+ATLAS_A_TR = {
+    "TL": "Alcyone_TR",
+    "TR": "Cygni_BL",
+    "BR": "Persei_BL",
+    "BL": "ATLAS_B_BL",
+}
+ATLAS_A_BR = {
+    "TL": "ATLAS_C_BR",
+    "TR": "Bootes_TR",
+    "BR": "Sirius_TR",
+    "BL": "Orion_BR",
+}
+ATLAS_A_BL = {
+    "TL": "Orion_TL",
+    "TR": "Sadatoni_BL",
+    "BR": "ATLAS_C_TL",
+    "BL": "Cygni_TR",
+}
+ATLAS_B_TL = {"TL": "Helvetios_BL", "TR": "Auriga_BR", "BR": "Maia_BR", "BL": "Maia_TL"}
+ATLAS_B_TR = {"TL": "Cygni_TR", "TR": "Bootes_TL", "BR": "Orion_BL", "BL": "Sirius_BR"}
+ATLAS_B_BR = {
+    "TL": "Sirius_TL",
+    "TR": "Maia_BL",
+    "BR": "ATLAS_C_BR",
+    "BL": "Eridani_TR",
+}
+ATLAS_B_BL = {
+    "TL": "ATLAS_A_TR",
+    "TR": "ATLAS_C_BR",
+    "BR": "Alcyone_TR",
+    "BL": "Maia_TR",
+}
+ATLAS_C_TL = {"TL": "Cygni_BL", "TR": "Orion_TL", "BR": "ATLAS_A_BL", "BL": "Aquila_BL"}
+ATLAS_C_TR = {
+    "TL": "Eridani_TL",
+    "TR": "Alcyone_BR",
+    "BR": "Auriga_TR",
+    "BL": "Auriga_BL",
+}
+ATLAS_C_BR = {
+    "TL": "ATLAS_A_BR",
+    "TR": "Persei_BR",
+    "BR": "ATLAS_B_BL",
+    "BL": "ATLAS_B_BR",
+}
+ATLAS_C_BL = {"TL": "Maia_TL", "TR": "Volantis_TL", "BR": "Orion_TR", "BL": "Bootes_TR"}
+Aquila_TL = {
+    "TL": "Helvetios_TR",
+    "TR": "Aquila_BR",
+    "BR": "Bootes_BR",
+    "BL": "Eridani_BR",
+}
+Aquila_TR = {
+    "TL": "Auriga_BL",
+    "TR": "Sadatoni_TL",
+    "BR": "Sadatoni_BR",
+    "BL": "Eridani_BL",
+}
+Aquila_BR = {"TL": "Bootes_TL", "TR": "Aquila_TL", "BR": "Sirius_TL", "BL": "Bootes_TL"}
+Aquila_BL = {
+    "TL": "Alcyone_BR",
+    "TR": "Orion_BR",
+    "BR": "ATLAS_C_TL",
+    "BL": "Eridani_TR",
+}
+Alcyone_TL = {"TL": "Alcyone_TR", "TR": "Cygni_TL", "BR": "Cygni_BL", "BL": "Bootes_TR"}
+Alcyone_TR = {
+    "TL": "ATLAS_B_BL",
+    "TR": "ATLAS_A_TR",
+    "BR": "Alcyone_TL",
+    "BL": "Helvetios_BR",
+}
+Alcyone_BR = {
+    "TL": "Helvetios_BR",
+    "TR": "Aquila_BL",
+    "BR": "Maia_BR",
+    "BL": "ATLAS_C_TR",
+}
+Alcyone_BL = {
+    "TL": "Sadatoni_BL",
+    "TR": "Sirius_BL",
+    "BR": "Helvetios_TL",
+    "BL": "Maia_TR",
+}
+Orion_TL = {"TL": "Auriga_BR", "TR": "ATLAS_C_TL", "BR": "Cygni_TR", "BL": "ATLAS_A_BL"}
+Orion_TR = {
+    "TL": "Maia_BR",
+    "TR": "ATLAS_C_BL",
+    "BR": "Helvetios_TL",
+    "BL": "Volantis_BR",
+}
+Orion_BR = {"TL": "Cygni_TL", "TR": "ATLAS_A_BR", "BR": "Aquila_BL", "BL": "Auriga_TR"}
+Orion_BL = {"TL": "ATLAS_B_TR", "TR": "Maia_TR", "BR": "Auriga_TL", "BL": "Sadatoni_BL"}
+Auriga_TL = {"TL": "Sirius_TR", "TR": "Orion_BL", "BR": "Persei_BL", "BL": "Persei_BR"}
+Auriga_TR = {"TL": "Maia_BL", "TR": "Orion_BR", "BR": "Bootes_BR", "BL": "ATLAS_C_TR"}
+Auriga_BR = {"TL": "Persei_TL", "TR": "Cygni_BL", "BR": "Orion_TL", "BL": "ATLAS_B_TL"}
+Auriga_BL = {
+    "TL": "Volantis_BR",
+    "TR": "Aquila_TR",
+    "BR": "ATLAS_C_TR",
+    "BL": "Volantis_TR",
+}
+Maia_TL = {
+    "TL": "Sadatoni_TL",
+    "TR": "ATLAS_C_BL",
+    "BR": "Eridani_BR",
+    "BL": "ATLAS_B_TL",
+}
+Maia_TR = {"TL": "Alcyone_BL", "TR": "Cygni_TL", "BR": "Orion_BL", "BL": "ATLAS_B_BL"}
+Maia_BR = {
+    "TL": "Alcyone_BR",
+    "TR": "ATLAS_B_TL",
+    "BR": "Orion_TR",
+    "BL": "Sadatoni_TR",
+}
+Maia_BL = {"TL": "Bootes_TR", "TR": "ATLAS_B_BR", "BR": "Sirius_BL", "BL": "Auriga_TR"}
+Bootes_TL = {
+    "TL": "ATLAS_B_TR",
+    "TR": "Bootes_BR",
+    "BR": "Aquila_BR",
+    "BL": "Aquila_BR",
+}
+Bootes_TR = {
+    "TL": "Alcyone_TL",
+    "TR": "ATLAS_A_BR",
+    "BR": "Maia_BL",
+    "BL": "ATLAS_C_BL",
+}
+Bootes_BR = {
+    "TL": "Auriga_TR",
+    "TR": "Volantis_TR",
+    "BR": "Bootes_TL",
+    "BL": "Aquila_TL",
+}
+Bootes_BL = {
+    "TL": "Eridani_TL",
+    "TR": "Helvetios_BL",
+    "BR": "Sadatoni_BL",
+    "BL": "ATLAS_A_TL",
+}
+Cygni_TL = {"TL": "Persei_BR", "TR": "Orion_BR", "BR": "Maia_TR", "BL": "Alcyone_TL"}
+Cygni_TR = {
+    "TL": "ATLAS_A_BL",
+    "TR": "ATLAS_B_TR",
+    "BR": "Orion_TL",
+    "BL": "Helvetios_TL",
+}
+Cygni_BR = {
+    "TL": "Eridani_BL",
+    "TR": "Sirius_BL",
+    "BR": "Sadatoni_TR",
+    "BL": "Persei_BL",
+}
+Cygni_BL = {
+    "TL": "Alcyone_TL",
+    "TR": "ATLAS_A_TR",
+    "BR": "Auriga_BR",
+    "BL": "ATLAS_C_TL",
+}
+Eridani_TL = {
+    "TL": "ATLAS_C_TR",
+    "TR": "Sirius_TL",
+    "BR": "Bootes_BL",
+    "BL": "Volantis_BL",
+}
+Eridani_TR = {
+    "TL": "Sirius_TL",
+    "TR": "ATLAS_B_BR",
+    "BR": "Aquila_BL",
+    "BL": "Sirius_BR",
+}
+Eridani_BR = {
+    "TL": "Persei_BR",
+    "TR": "Sadatoni_BR",
+    "BR": "Aquila_TL",
+    "BL": "Maia_TL",
+}
+Eridani_BL = {
+    "TL": "Volantis_BR",
+    "TR": "Aquila_TR",
+    "BR": "Sirius_TR",
+    "BL": "Cygni_BR",
+}
+Helvetios_TL = {
+    "TL": "Orion_TR",
+    "TR": "Helvetios_BR",
+    "BR": "Cygni_TR",
+    "BL": "Alcyone_BL",
+}
+Helvetios_TR = {
+    "TL": "Aquila_TL",
+    "TR": "ATLAS_A_TL",
+    "BR": "Volantis_BL",
+    "BL": "Persei_TL",
+}
+Helvetios_BR = {
+    "TL": "Alcyone_TR",
+    "TR": "Volantis_TL",
+    "BR": "Alcyone_BR",
+    "BL": "Helvetios_TL",
+}
+Helvetios_BL = {
+    "TL": "Persei_TR",
+    "TR": "ATLAS_B_TL",
+    "BR": "Bootes_BL",
+    "BL": "Sirius_BL",
+}
+
+locals_keys = list(locals().keys())[10:]
+mega_dict = {name: globals()[name] for name in locals_keys}
+
+# with open("data.json", "w", encoding="utf-8") as json_file:
+#     json.dump(mega_dict, json_file, ensure_ascii=False, indent=4)
+print(mega_dict["Helvetios_BL"])
