e512c19c5433db932d58bebee96798bb89b5cc18
alexeyBolokhov <bolokhov.ae@gmail.com>
2024-12-08 14:55:08 +0500

    08/12 inf

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    if second == 0:
+        rez = "Ошибка"
+        return rez
+    else:
+        rez = first / second
+        return rez
+
+# result1 = divide(69, 3)
+# print(result1)
---
+++ true_math.py
@@ -0,0 +1,14 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        float('inf')
+        return inf
+    else:
+        rez = first / second
+        return rez
+
+
+# result1 = divide(69, 3)
+# print(result1)
+# result2 = divide(3, 0)
+# print(result2)
